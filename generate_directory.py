8d630e4d02a13f231981f3eaff75e67efd97d004
Teaching <joey.orofino@gmail.com>
2024-09-23 23:58:20 -0400

    changed dockerfile

--- generate_directory.py
+++ generate_directory.py
@@ -11,6 +11,6 @@ for env in envs[0:3]:
     basefile = os.path.basename(env)
     os.makedirs(newdir, exist_ok=True)
     shutil.copyfile(env, os.path.join(newdir, basefile))
-    with open('Dockerfile', 'rt') as f, open(os.path.join(newdir, 'Dockerfile'), 'w') as w:
+    with open('template.txt', 'rt') as f, open(os.path.join(newdir, 'Dockerfile'), 'w') as w:
         for line in f:
             w.write(line.replace('<env_desc>', basefile))
